9446c7d431f1fa57eb027c6e105621c0ae151028
unknown <shashwatshah02@gmail.com>
2023-06-20 13:30:53 +0530

    Address bar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,6 +14,15 @@ export default function Footer() {
   const close = ()=>{
     setOpen(false);
   }
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/64906664cc26a871b023747c/1h3a0vv6u';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
   return (
     <div
       style={{
