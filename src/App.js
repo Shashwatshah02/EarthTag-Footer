a19012a66b3c8cfd62a5bc55c2e1010aab4707e6
unknown <shashwatshah02@gmail.com>
2023-07-04 13:05:29 +0530

    Google Calendar Events

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Footer />
-      <a href="https://calendar.google.com/calendar/u/0?cid=MDlmYWVlMzk1MzEwZTJmZjczNTlhODM2YTEwZTYwNGM1NjRjNmJhMTcyNzRkMjA3YWIzYmVjN2JjMzNiOTQzM0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t"><button>Add To your Calander</button></a>
+      <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NTR2cjZqajc5N204bDMzZmIxZGYzY2I4ODUgMDlmYWVlMzk1MzEwZTJmZjczNTlhODM2YTEwZTYwNGM1NjRjNmJhMTcyNzRkMjA3YWIzYmVjN2JjMzNiOTQzM0Bn&tmsrc=09faee395310e2ff7359a836a10e604c564c6ba17274d207ab3bec7bc33b9433%40group.calendar.google.com"><button>Add To your Calander</button></a>
     </div>
   );
 }
